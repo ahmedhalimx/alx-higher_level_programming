97ea2b8a28d85c9835a434c8fe79f048f60d6747
Ahmed Halim <ahmedhalimcs101@gmail.com>
2023-06-12 14:24:21 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     truth_set = []
-    for i in (len(my_list) - 1):
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             truth_set.append(True)
         else:
