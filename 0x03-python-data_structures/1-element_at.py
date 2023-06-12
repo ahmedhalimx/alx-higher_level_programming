254e836c6beec2496890d1ae9292fa53b024d28f
Ahmed Halim <ahmedhalimcs101@gmail.com>
2023-06-12 13:11:33 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > range(my_list):
         return None
-    for i in range(my_list)
+    for i in range(my_list):
         if idx == i:
             return my_list[i]
