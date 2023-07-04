07fc517d71be6e2c347450823285ac7711bcb956
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-07-04 23:40:18 +0300

    0x09. Python - Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Definition of a locked class."""
+
+
+class LockedClass:
+    """
+    Allows the user to only instantiate the
+    'first_name' attribute.
+    """
+
+    __slots__ = ["first_name"]
