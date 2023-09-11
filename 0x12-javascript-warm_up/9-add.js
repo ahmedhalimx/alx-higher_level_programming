b8d5084f24333a5d2a0854677ebfd114e6873f4d
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-11 20:20:33 +0300

    9-add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+function add(a, b) {
+	return (+a + +b);
+}
+
+if (isNaN(argv[2]) || isNaN(argv[3])) {
+	console.log("NaN");
+} else {
+	console.log(add(argv[2], argv[3]));
+}
