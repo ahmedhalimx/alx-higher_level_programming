675e0cf1b534b4ed6113e5a34e02b4b018dcdd84
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-11 20:21:09 +0300

    boring standard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-function add(a, b) {
-	return (+a + +b);
+function add (a, b) {
+  return (+a + +b);
 }
 
 if (isNaN(argv[2]) || isNaN(argv[3])) {
-	console.log("NaN");
+  console.log('NaN');
 } else {
-	console.log(add(argv[2], argv[3]));
+  console.log(add(argv[2], argv[3]));
 }
