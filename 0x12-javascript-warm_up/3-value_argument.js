9293e928d265d7a7e38a85b7b7d7ddf3758f2fe4
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-11 19:32:04 +0300

    fourth task

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  for (let i = 2; argv[i] !== undefined; ++i) {
+    console.log(argv[i]);
+  }
+}
