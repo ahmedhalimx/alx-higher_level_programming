3cf70380d71e4c26453ed378c6a0b5d99aeeebd1
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-11 20:50:06 +0300

    standard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ let argv = process.argv;
 if (argv.length <= 3) {
   console.log(0);
 } else {
-   argv = argv.map(Number)
+  argv = argv.map(Number)
     .slice(2, argv.length)
     .sort((a, b) => a - b);
   console.log(argv[argv.length - 2]);
