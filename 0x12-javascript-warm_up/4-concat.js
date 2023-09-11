0ccba12be2b37b6ea8d9d8f1cfd17eb08dbad82f
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-11 19:41:02 +0300

    fixed fifth solution

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(`${argv[2]} is ${argv[3]}`);
-}
+console.log(`${argv[2]} is ${argv[3]}`);
