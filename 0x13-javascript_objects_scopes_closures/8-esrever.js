2b5d9cbcde8278c9121058c49ae3cc81f7e9948a
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-12 20:01:11 +0300

    8-esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,7 @@ exports.esrever = function (list) {
   const midOfList = Math.ceil(list.length / 2);
   let endOfList = list.length - 1;
   for (let i = 0; i !== midOfList; ++i) {
-    let temp = list[i];
+    const temp = list[i];
     list[i] = list[endOfList];
     list[endOfList] = temp;
     --endOfList;
