c4dbd155ff4fd05de83ed3c0095d194fee9cae53
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-09-16 17:17:11 +0300

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-    A script that lists all data from the database hbtn_0e_0_usa
+a script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
 """
 
 import sys
@@ -15,8 +16,13 @@ if __name__ == "__main__":
                          host='localhost',
                          port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    instruction = f"SELECT * FROM states\
+            WHERE name LIKE BINARY '{sys.argv[4]}'\
+            ORDER BY id ASC"
+
+    cursor.execute(instruction)
     data = cursor.fetchall()
+
     for row in data:
         print(row)
 
